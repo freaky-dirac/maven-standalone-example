f599ffc0d01d7002066bced3f2361ec626af226e
Raffaele Bua <raffaele.bua@gmail.com>
2015-10-11 13:01:53 +0200

    entry point

---
+++ src/main/java/eu/freakydirac/application/App.java
@@ -0,0 +1,12 @@
+package eu.freakydirac.application;
+
+
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("----------");
+        System.out.println("Hello world");
+    }
+
+}
